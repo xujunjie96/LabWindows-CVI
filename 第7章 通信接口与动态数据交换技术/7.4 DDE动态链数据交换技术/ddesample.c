0fe5e99caa0bbbf3ec58d40165d7f626d8d9ba3c
xujunjie96 <xujunjie0396@163.com>
2018-05-21 15:17:46 +0800

    5/20ddeupdate

--- 第7章 通信接口与动态数据交换技术/7.4 DDE动态链数据交换技术/ddesample.c
+++ 第7章 通信接口与动态数据交换技术/7.4 DDE动态链数据交换技术/ddesample.c
@@ -1,3 +1,9 @@
+#include <analysis.h>
+#include <ddesupp.h>
+#include <utility.h>
+#include <ansi_c.h>
+#include "toolbox.h"
+#include <formatio.h>
 #include <cvirte.h>		
 #include <userint.h>
 #include "ddesample.h"
@@ -34,7 +40,6 @@ int CVICALLBACK panelCB (int panel, int event, void *callbackData,
 				 ClientDDEExecute (datalink, "[SAVE()]", 0); //保存excel文件
 				 ClientDDEExecute (datalink, "[QUIT()]", 0); //退出excel
 				 DisconnectFromDDEServer (datalink);	 //断开DDE连接
-				 
 			}
 			QuitUserInterface (0);
 			break;
@@ -52,7 +57,7 @@ int CVICALLBACK startexcel (int panel, int control, int event,
 		case EVENT_COMMIT:
 			GetDir (prjdir);
 			strcat(prjdir,"\\wjx.xls");
-			OpenDocumentInDefaultViewer (prjdie, VAL_NO_ZOOM);
+			OpenDocumentInDefaultViewer (prjdir, VAL_NO_ZOOM);
 			SetInputMode (panelHandle, PANEL_CMD_START, 0);   //按钮变暗 不可点击
 			break;
 	}
@@ -67,6 +72,7 @@ int CVICALLBACK linkexcel (int panel, int control, int event,
 	char testbuf[50];
 	double value;
 	char databuf[100];
+	char lineitem[20];
 	char line[10];
 	int i,j;
 	switch (event)
@@ -82,7 +88,7 @@ int CVICALLBACK linkexcel (int panel, int control, int event,
 			{
 				for(j=1;j<=10;j++)
 				{
-					lineitem='\0';
+					lineitem[0]='\0';
 					line[0]='\0';
 					value=0.0;
 					strcat(lineitem,"R");
@@ -92,26 +98,94 @@ int CVICALLBACK linkexcel (int panel, int control, int event,
 					line[0]='\0';
 					Fmt(line,"%s<%d",j);
 					strcat(lineitem,line);
-					ClientDDERead (datalink, lineitem, CF_TEXT, databuf, sizeof(databuf), 0);
+					ClientDDERead (datalink, lineitem, CF_TEXT, databuf, sizeof(databuf), 0);   //从DDE服务器读取数据
+					Fmt(&value,"%f<%s",databuf);
+					SetTableCellVal (panelHandle, PANEL_TABLE, MakePoint(j,i), value);	  //将数据写入表格中
+					
 				}
 			}
 			break;
-	}																	  .
+	}																	  
 	
+	return 0;
+}
+
+//声明DDE回调函数
+int clientCB(unsigned handle,char *topicName, char *itemName,int xType,int dataFmt,int dataSize,void *dataPtr,void *callbackData)   
+{
+	double value;
+	switch (xType)
+	{
+		case DDE_DISCONNECT:
+			DisableBreakOnLibraryErrors();
+			DisconnectFromDDEServer (datalink);
+			break;
+		case DDE_DATAREADY:
+			Fmt(&value,"%f<%s",(char*)dataPtr);//获得DDE连接第一行第一列的数据
+			SetCtrlVal (panelHandle, PANEL_CELLVALUE, value);
+			break;
+	}
 	return 0;
 }
 
 int CVICALLBACK wavesignal (int panel, int control, int event,
 							void *callbackData, int eventData1, int eventData2)
 {
+	char databuf[10000];
+	char testbuf[100];
+	double wave[100];
+	double sinephase=0;
+	double cosphase=90.0;
+	double squarephase=90.0;
+	int wavemode;
 	switch (event)
 	{
 		case EVENT_COMMIT:
-
-			break;
-		case EVENT_VAL_CHANGED:
+			GetCtrlVal (panelHandle, PANEL_RINGSLIDE, &wavemode);	
+			switch(wavemode)
+			{
+				case 1:
+					SineWave (100, 1.0, 0.01, &sinephase, wave);
+					ddewave(databuf,testbuf,wave);
+					break;
+				case 2:
+					SineWave (100, 1.0, 0.01, &cosphase, wave);
+					ddewave(databuf,testbuf,wave);
+					break;
+				case 3:
+					Triangle (100, 1.0, wave);
+					ddewave(databuf,testbuf,wave);
+					break;
+				case 4:
+					SquareWave (100, 1.0, 0.01, &squarephase, 50.0, wave);
+					ddewave(databuf,testbuf,wave);
+					break;
+				case 5:
+					Pulse (100, 1.0, 0, 1, wave);
+					
+					ddewave(databuf,testbuf,wave);
+					break;
+			}
+			
 
 			break;
 	}
 	return 0;
+}
+
+//自定义函数 向excel写数据     											   
+void ddewave(char databuf[],char testbuf[],double wave[])
+{
+	int i;
+	
+	databuf[0]='\0';
+	testbuf[0]='\0';
+	for(i=0;i<100;i++)
+	{
+		Fmt(testbuf,"%s<%f",wave[i]);
+		strcat(databuf,testbuf);
+		strcat(databuf,"\n");
+	}
+	ClientDDEWrite (datalink, "R1C1:R100C1", CF_TEXT, databuf, strlen(databuf), 0); //向dde服务器写数据
+	linkexcel(panelHandle,PANEL_CMD_LINK,EVENT_COMMIT,NULL,0,0);  //刷新数据
 }
